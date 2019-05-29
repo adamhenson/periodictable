1237f78ecb4d511808739556c7fe70ad0f5b7b42
Adam Henson <adamhenson1979@gmail.com>
2019-05-29 13:33:14 -0400

    data.

--- src/data.js
+++ src/data.js
@@ -90,4 +90,159 @@ export default [
       relativeAtomicMass: 226.03,
     },
   ],
+
+  // group 3
+  [
+    {/* empty space */},
+    {/* empty space */},
+    {/* empty space */},
+    {
+      name: 'Scandium',
+      symbol: 'Sc',
+      atomicNumber: 21,
+      relativeAtomicMass: 44.956,
+    },
+    {
+      name: 'Yttrium',
+      symbol: 'Y',
+      atomicNumber: 39,
+      relativeAtomicMass: 88.906,
+    },
+    {
+      name: 'Lutetium',
+      symbol: 'Lu',
+      atomicNumber: 71,
+      relativeAtomicMass: 174.97,
+    },
+    {
+      name: 'Lawrencium',
+      symbol: 'Lr',
+      atomicNumber: 103,
+      relativeAtomicMass: 262.11,
+    },
+  ],
+
+  // group 4
+  [
+    {/* empty space */},
+    {/* empty space */},
+    {/* empty space */},
+    {
+      name: 'Titanium',
+      symbol: 'Ti',
+      atomicNumber: 22,
+      relativeAtomicMass: 47.867,
+    },
+    {
+      name: 'Zirconium',
+      symbol: 'Zr',
+      atomicNumber: 40,
+      relativeAtomicMass: 91.224,
+    },
+    {
+      name: 'Hafnium',
+      symbol: 'Hf',
+      atomicNumber: 72,
+      relativeAtomicMass: 178.49,
+    },
+    {
+      name: 'Rutherfordium',
+      symbol: 'Rf',
+      atomicNumber: 104,
+      relativeAtomicMass: 267.12,
+    },
+  ],
+
+  // group 5
+  [
+    {/* empty space */},
+    {/* empty space */},
+    {/* empty space */},
+    {
+      name: 'Vanadium',
+      symbol: 'V',
+      atomicNumber: 23,
+      relativeAtomicMass: 50.942,
+    },
+    {
+      name: 'Niobium',
+      symbol: 'Nb',
+      atomicNumber: 41,
+      relativeAtomicMass: 92.906,
+    },
+    {
+      name: 'Tantalum',
+      symbol: 'Ta',
+      atomicNumber: 73,
+      relativeAtomicMass: 180.95,
+    },
+    {
+      name: 'Dubnium',
+      symbol: 'Db',
+      atomicNumber: 105,
+      relativeAtomicMass: 270.13,
+    },
+  ],
+
+  // group 6
+  [
+    {/* empty space */},
+    {/* empty space */},
+    {/* empty space */},
+    {
+      name: 'Chromium',
+      symbol: 'Cr',
+      atomicNumber: 24,
+      relativeAtomicMass: 51.996,
+    },
+    {
+      name: 'Molybdenum',
+      symbol: 'Mo',
+      atomicNumber: 42,
+      relativeAtomicMass: 95.95,
+    },
+    {
+      name: 'Tungsten',
+      symbol: 'W',
+      atomicNumber: 74,
+      relativeAtomicMass: 183.84,
+    },
+    {
+      name: 'Seaborgium',
+      symbol: 'Sg',
+      atomicNumber: 106,
+      relativeAtomicMass: 269.13,
+    },
+  ],
+
+  // group 7
+  [
+    {/* empty space */},
+    {/* empty space */},
+    {/* empty space */},
+    {
+      name: 'Manganese',
+      symbol: 'Mn',
+      atomicNumber: 25,
+      relativeAtomicMass: 54.938,
+    },
+    {
+      name: 'Technetium',
+      symbol: 'Tc',
+      atomicNumber: 43,
+      relativeAtomicMass: 96.906,
+    },
+    {
+      name: 'Rhenium',
+      symbol: 'Re',
+      atomicNumber: 75,
+      relativeAtomicMass: 186.21,
+    },
+    {
+      name: 'Bohrium',
+      symbol: 'Bh',
+      atomicNumber: 107,
+      relativeAtomicMass: 270.13,
+    },
+  ],
 ];
