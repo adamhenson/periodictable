ee62054af4db49a057099e6d3d7aaf4a2c44a731
Adam Henson <adamhenson1979@gmail.com>
2019-07-16 19:34:35 -0400

    typescript.

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <Header />
+      <p>hello world</p>
+    </div>
+  );
+}
+
+export default App;
